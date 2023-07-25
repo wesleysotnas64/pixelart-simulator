bcb40f38858629b6107da9a381215689e7cbf8f4
Wesley Santos <wesleysotnas64@gmail.com>
2023-07-24 22:18:52 -0300

    feat: Ferramentas de auxílio de câmera e material.

---
+++ Scripts/CameraController.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Vector3 initialPosition;
+    private Transform eye;
+    public  Transform target;
+    public float velocity;
+
+    void Start()
+    {
+        eye = GetComponent<Transform>();
+        // at.position = new Vector3(0,0,0);
+        initialPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("d")) HorizontalMove(1);
+        if(Input.GetKey("a")) HorizontalMove(-1);
+        if(Input.GetKey("w")) VerticalMove(1);
+        if(Input.GetKey("s")) VerticalMove(-1);
+
+        FrontMove(Input.mouseScrollDelta);
+
+        if(Input.GetKeyDown("r")) ResetPosition();
+
+
+        LookAt();
+    }
+
+    private void ResetPosition()
+    {
+        transform.position = initialPosition;
+    }
+
+    private void LookAt()
+    {
+        Vector3 direction = (target.position - eye.position);
+        eye.forward = direction;
+    }
+
+    private void HorizontalMove(int axe = 1)
+    {
+        eye.Translate(axe * velocity * Time.deltaTime, 0, 0);
+    }
+
+    private void VerticalMove(int axe = 1)
+    {
+        eye.Translate(0, axe * velocity * Time.deltaTime, 0);
+    }
+
+    private void FrontMove(Vector2 v)
+    {
+        eye.Translate(0, 0, velocity * Time.deltaTime * v.y);
+    }
+}
---
+++ Scripts/MaterialController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialController : MonoBehaviour
+{
+    public List<MeshRenderer> materialList;
+
+    [Range(0, 1)]
+    public float ambientStrength;
+
+    [Range(0, 1)]
+    public float ambientStrengthReflectivity;
+
+    [Range(0, 1)]
+    public float lightStrength;
+
+    [Range(0, 1)]
+    public float difuseStrengthReflectivity;
+
+    [Range(0, 1)]
+    public float specularStrengthReflectivity;
+
+    [Range(1, 256)]
+    public float shininess;
+
+    private void Start()
+    {
+        ambientStrength = 0.1f;
+        ambientStrengthReflectivity = 0.1f;
+        lightStrength = 1.0f;
+        difuseStrengthReflectivity = 0.5f;
+        specularStrengthReflectivity = 0.8f;
+        shininess = 32;
+    }
+
+    void Update()
+    {
+        SetMaterialsParameters();
+    }
+
+    private void SetMaterialsParameters()
+    {
+        foreach(MeshRenderer material in materialList)
+        {
+            material.material.SetFloat("_AmbientStrength", ambientStrength);
+            material.material.SetFloat("_AmbientStrengthReflectivity", ambientStrengthReflectivity);
+            material.material.SetFloat("_LightStrength", lightStrength);
+            material.material.SetFloat("_DifuseStrengthReflectivity", difuseStrengthReflectivity);
+            material.material.SetFloat("_SpecularStrengthReflectivity", specularStrengthReflectivity);
+
+            try
+            {
+                material.material.SetFloat("_Shininess", shininess);
+            }
+            catch{}
+        }
+    }
+}
