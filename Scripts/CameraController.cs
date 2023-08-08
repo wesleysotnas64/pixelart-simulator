bd09ad1b3dbccbff29afa25da92cb74f92e7e20e
Wesley Santos <wesleysotnas64@gmail.com>
2023-08-07 22:10:22 -0300

    Reajuste de controles auxiliares.

--- Scripts/CameraController.cs
+++ Scripts/CameraController.cs
@@ -2,56 +2,36 @@
 
 public class CameraController : MonoBehaviour
 {
-    private Vector3 initialPosition;
-    private Transform eye;
-    public  Transform target;
-    public float velocity;
+    public float rotationSpeed = 5f;  // Velocidade de rotação da câmera
+    public Transform target;         // Ponto ao redor do qual a câmera irá se mover
+    public float distance = 10f;     // Distância da câmera ao ponto
 
-    void Start()
+    public float currentLatitude = 0f;
+    public float currentLongitude = 0f;
+
+    private void Start()
     {
-        eye = GetComponent<Transform>();
-        initialPosition = transform.position;
+        Vector3 position = Quaternion.Euler(currentLatitude, currentLongitude, 0) * new Vector3(0, 0, -distance);
+        transform.position = target.position + position;
+        transform.LookAt(target.position);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(Input.GetKey("d")) HorizontalMove(1);
-        if(Input.GetKey("a")) HorizontalMove(-1);
-        if(Input.GetKey("w")) VerticalMove(1);
-        if(Input.GetKey("s")) VerticalMove(-1);
+        if(Input.mouseScrollDelta.y != 0)
+            distance -= Input.mouseScrollDelta.y;
 
-        FrontMove(Input.mouseScrollDelta);
+        if (Input.GetMouseButton(1))
+        {
+            currentLatitude -= Input.GetAxis("Mouse Y") * rotationSpeed;
+            currentLongitude += Input.GetAxis("Mouse X") * rotationSpeed;
 
-        if(Input.GetKeyDown("r")) ResetPosition();
+            // Limitar a latitude entre -90 e 90 graus para evitar inversões
+            currentLatitude = Mathf.Clamp(currentLatitude, -90f, 90f);
 
-
-        LookAt();
-    }
-
-    private void ResetPosition()
-    {
-        transform.position = initialPosition;
-    }
-
-    private void LookAt()
-    {
-        Vector3 direction = (target.position - eye.position);
-        eye.forward = direction;
-    }
-
-    private void HorizontalMove(int axe = 1)
-    {
-        eye.Translate(axe * velocity * Time.deltaTime, 0, 0);
-    }
-
-    private void VerticalMove(int axe = 1)
-    {
-        eye.Translate(0, axe * velocity * Time.deltaTime, 0);
-    }
-
-    private void FrontMove(Vector2 v)
-    {
-        eye.Translate(0, 0, velocity * Time.deltaTime * v.y);
+        }
+        Vector3 position = Quaternion.Euler(currentLatitude, currentLongitude, 0) * new Vector3(0, 0, -distance);
+        transform.position = target.position + position;
+        transform.LookAt(target.position);
     }
 }
